c02e56d3edfc4c904eb97bc6809d891d371042f5
ntsatsura <it.tester.qa@gmail.com>
2024-05-10 02:34:33 +0200

    Corrected hw-2.2.1

--- calculations.js
+++ calculations.js
@@ -1,25 +1,30 @@
 function increaseBalance(x, y) {
-  x = x + y;
-  return x;
+  let x1 = x + y;
+  return x1;
 }
 
-function decreaseBalance(x) {
-x = x - y;
-return x;
+function decreaseBalance(x, y) {
+let x2 = x - y;
+return x2;
 }
 
-function divideBalanceByAccounts(x, y) {
-  x = x / y;
-  return x;
-  console.log("x");
+function divideBalanceByAccounts(x, y, z) {
+let balance = x + z 
+let x3 = balance /= y;
+return x3;
 }
 
 function getRestAfterDivision(x, y) {
-  x = x % y;
-  return x;
+  let x4 = x % y;
+  return x4;
 }
 
-console.log(increaseBalance(3000, 700));
-console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
-console.log(getRestAfterDivision(7000, 3));
+let resultIncreaseBalance = increaseBalance(3000, 700);
+let resultDecreaseBalance = decreaseBalance(3000, 700);
+let resultDivideBalanceByAccounts = divideBalanceByAccounts(3000, 2, 4000);
+let resultGetRestAfterDivision = getRestAfterDivision(7000, 3);
+
+console.log(resultIncreaseBalance);
+console.log(resultDecreaseBalance);
+console.log(resultDivideBalanceByAccounts);
+console.log(resultGetRestAfterDivision);
