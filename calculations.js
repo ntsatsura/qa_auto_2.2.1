006b714b6f80d2b4fd65a272342186237447ba60
ntsatsura <it.tester.qa@gmail.com>
2024-05-11 00:57:09 +0200

    Corrected-2 hw-2.2.1

--- calculations.js
+++ calculations.js
@@ -1,52 +1,25 @@
 function increaseBalance(x, y) {
-  x += y;
+  x = x + y;
   return x;
 }
 
 function decreaseBalance(x, y) {
- x -= y;
+x = x - y;
 return x;
 }
 
 function divideBalanceByAccounts(x, y, z) {
-let balance = x + z
-balance /= y;
-return balance;
-}
-
-function getRestAfterDivision(x, y) {
-x %= y;
+  x = x / y;
+console.log(x, "this is x");
   return x;
 }
 
-let resultIncreaseBalance = increaseBalance(3000, 700);
-let resultDecreaseBalance = decreaseBalance(3000, 700);
-let resultDivideBalanceByAccounts = divideBalanceByAccounts(3000, 2, 4000);
-let resultGetRestAfterDivision = getRestAfterDivision(7000, 3);
+function getRestAfterDivision(x, y) {
+  x = x % y;
+  return x;
+}
 
-console.log(resultIncreaseBalance);
-console.log(resultDecreaseBalance);
-console.log(resultDivideBalanceByAccounts);
-console.log(resultGetRestAfterDivision);
-
-
-// пример инкремента 1
-let inc1 = 5;
-inc1++;
-console.log(inc1); 
-
-// пример инкремента 2
-let inc2 = 5;
-++inc2;
-console.log(inc2); 
-
-
-// пример декремента 1
-let decr1 = 5;
-decr1--;
-console.log(decr1); 
-
-// пример декремента 1
-let decr2 = 5;
---decr2;
-console.log(decr2); 
+console.log(increaseBalance(3000, 700));
+console.log(decreaseBalance(3000, 700));
+console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(getRestAfterDivision(7000, 3));
