7b8a70952ee59abebfd8a73c2e22ef4fe4093df6
ntsatsura <it.tester.qa@gmail.com>
2024-05-11 01:06:02 +0200

    Corrected-2 hw-2.2.4

--- calculations.js
+++ calculations.js
@@ -19,3 +19,23 @@ console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2, 4000));
 console.log(getRestAfterDivision(7000, 3));
+
+// пример инкремента 1
+let inc1 = 5;
+inc1++;
+console.log(inc1);
+
+// пример инкремента 2
+let inc2 = 5;
+++inc2;
+console.log(inc2);
+
+// пример декремента 1
+let decr1 = 5;
+decr1--;
+console.log(decr1);
+
+// пример декремента 1
+let decr2 = 5;
+--decr2;
+console.log(decr2);
