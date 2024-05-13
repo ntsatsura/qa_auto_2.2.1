a236e51795bd4da4ebeb58eeb7a86da2ad351c37
ntsatsura <it.tester.qa@gmail.com>
2024-05-13 23:08:01 +0200

    Corrected-3 hw-2.2.2

--- calculations.js
+++ calculations.js
@@ -6,8 +6,8 @@ function decreaseBalance(x, y) {
   return (x -= y);
 }
 
-function divideBalanceByAccounts(x, y, z) {
-  console.log(x, "this is x");
+function divideBalanceByAccounts(x, y) {
+ // console.log(x, "this is x");
   return (x /= y);
 }
 
@@ -17,37 +17,6 @@ function getRestAfterDivision(x, y) {
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
 
-// пример инкремента 1
-function exampleIncrement1(inc1) {
-  inc1++;
-  return inc1;
-}
-
-console.log(exampleIncrement1(5));
-
-// пример инкремента 2
-function exampleIncrement2(inc2) {
-  ++inc2;
-  return inc2;
-}
-
-console.log(exampleIncrement2(5));
-
-// пример декремента 1
-function exampleDecrement1(decr1) {
-  decr1--;
-  return decr1;
-}
-
-console.log(exampleDecrement1(5));
-
-// пример декремента 2
-function exampleDecrement2(decr2) {
-  --decr2;
-  return decr2;
-}
-
-console.log(exampleDecrement2(5));
